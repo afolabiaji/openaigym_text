603f027daca17f7bda4a4dff79697fe23f718431
afolabiaji <af.ajibade@gmail.com>
2021-08-18 22:32:13 +0100

    changed rewards and punishment during training

--- openai_frozen_lake.py
+++ openai_frozen_lake.py
@@ -16,7 +16,7 @@ print("State Size", state_size)
 q_table = np.zeros((state_size, action_size))
 
 # 3: Hyperparameters
-episodes = 10**7
+episodes = 50000
 learning_rate = 0.1
 discount = 0.9
 
@@ -26,11 +26,14 @@ epsilon = 1.0
 max_epsilon = 1.0
 min_epsilon = 0.01
 decay_rate = 0.01 
+step_limit = 50
+goal_count = 0
 
 # 4: Training
 for episode in range(episodes):
     done = False
     state= env.reset()
+    step_count = 0
     while done == False:
         exp_exp_tradeoff = np.random.uniform(0,1)
         if exp_exp_tradeoff > epsilon:
@@ -39,10 +42,21 @@ for episode in range(episodes):
             action = env.action_space.sample()
         
         new_state, reward, done, info = env.step(action)
-        # print(reward, done)
+        env.render()
+        if new_state == 12 or new_state == 5 or new_state == 7 or new_state == 11:
+            reward = -10
+        if step_count > step_limit:
+            reward = -1
+            done = True
+        if new_state == 15:
+            goal_count += 1
+
+        print(goal_count)
         q_table[state, action] = q_table[state, action] + learning_rate*(reward + discount*(np.max(q_table[new_state, :])) - q_table[state, action])
 
         state = new_state
+
+        step_count += 1
 
     epsilon = min_epsilon + (max_epsilon - min_epsilon)*np.exp(-decay_rate*episode)
 
@@ -50,7 +64,7 @@ for episode in range(episodes):
 total_reward = 0
 
 
-for episode in range(5):
+for episode in range(10000):
     state = env.reset()
     done = False
     while done == False:
@@ -62,4 +76,8 @@ for episode in range(5):
         state = new_state
         total_reward += reward
 
-print(f"Q Table final reward = {total_reward/5}")
+print(f"Q Table Accuracy = {total_reward/10000}")
+
+#I've tweaked hyperparameters in training. It seems algorithm learns quickest when punished harsly for falling into hole and punished for taking too long to move
+#Punishment for taking long balances out punishment applied to surronding tiles of holes (these take a hit to q value during exploration)
+#Balance between reward/punishment/max_steps seems to dictacte how quickly algorithm learns
